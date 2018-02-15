24f4cb53845e4bb772a08c79f0724828250657ce
hatemanik <hatemanik@yandex.ru>
2018-02-15 14:57:12 +0300

    выполнение ДЗ, но сервер постоянно отдает что время вышло или коннект не установлен

--- hw-2/js/script.js
+++ hw-2/js/script.js
@@ -8,9 +8,6 @@ xhr.send();
 xhr.onreadystatechange = function(){
     if(xhr.readyState === XMLHttpRequest.DONE){
         if(xhr.status === 200) {
-            // сервер возвращает json-объект, массив с покемонами храниться в свойстве results
-            // можно посмотреть структуру возвращаемого объета через console.log(), так:
-            console.log(JSON.parse(xhr.responseText));
             var pokemons = JSON.parse(xhr.responseText).results;
             var ul = document.createElement('ul');
             for(var i = 0; i < pokemons.length; i++) {
@@ -18,8 +15,6 @@ xhr.onreadystatechange = function(){
                 var image = document.createElement('img');
                 var span = document.createElement('span');
                 span.textContent = pokemons[i].name;
-                // тут вобщем правильно, только тогда придется пихать картинки в корень проекта,
-                // лучше создать папку img и запихать туда, только тогда ниже надо будет путь исправить
                 image.src = 'img/'+(i + 1) + '.png';
                 li.appendChild(span);
                 li.appendChild(image);
